71c114c15155afba2eb7392dbe7524b81681c0c0
aninha306 <ana-pontes@aluno.senai.br>
2023-11-23 10:10:47 -0300

    .

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import rotasArtes from "./arte.routes.js";
+import rotasSobreNos from "./sobrenos.routes.js"
 
 const router = Router()
 
 router.use("/artes", rotasArtes); //Rota para puxar as artes
+router.use("/sobrenos", rotasSobreNos); //Rota para puxar os integrantes
 
 router.get("/", (req, res) => {
     return res.status(200).send({ message: "Rota está funcionando!" }); //Rota para ver se o servidor está funcionando
