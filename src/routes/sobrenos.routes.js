def874504038b953396c51be8d6c47eca5da7146
Luana Fassini <luanafassini.564@gmail.com>
2023-11-23 14:40:12 -0300

    feat: create page sobrenos.routes

--- src/routes/sobrenos.routes.js
+++ src/routes/sobrenos.routes.js
@@ -1,1 +1,11 @@
 import { Router } from "express";
+import { getTodosIntegrantes, getIntegrantesByid,editarIntegrante,deletarIntegrante,adicionarIntegrantes } from "../controllers/sobrenos.controller.js";
+
+
+const routes = new Router();
+
+routes.get("/", getTodosIntegrantes);
+routes.get("/:id", getIntegrantesByid);
+routes.put("/:id",editarIntegrante);
+routes.delete("/:id", deletarIntegrante);
+routes.post("/", adicionarIntegrantes);
