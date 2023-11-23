9c417a98635261f827ccb6e4de5afc58f0a076b8
aninha306 <ana-pontes@aluno.senai.br>
2023-11-23 13:50:31 -0300

    .

--- src/models/sobrenos.js
+++ src/models/sobrenos.js
@@ -1,1 +1,10 @@
-import {v4 as uuidv4 }from "uuid";
+import {v4 as uuidv4 }from "uuid"
+class integrantes{
+    constructor(nome, idade, imagem){
+        this.nome =nome;
+        this.idade =idade;
+        this.imagem=imagem; 
+        this.id=uuidv4()
+    }
+}
+export default integrantes;
