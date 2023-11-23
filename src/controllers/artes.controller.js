d801463e1d301b897d1579e9f94c53195d4175c8
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-11-23 09:10:43 -0300

    .

--- src/controllers/artes.controller.js
+++ src/controllers/artes.controller.js
@@ -1,6 +1,8 @@
-import {ArteList} from "../models/ArteList.js"
+import {resgataArtes} from "../models/ArteList.js"
 
-const arte = new ArteList()
+const arte = new resgataArtes()
+
+//Função de verificação de imagem
 function verifyImage(url) {
     var allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp'];
   
@@ -8,6 +10,8 @@ function verifyImage(url) {
   
     return allowedExtensions.includes(extension);
   }
+
+  //Função de obter todas as artes
 export const getTodasArtes = (req , res) => {
 
     let artes = arte.getTodasArtes();
@@ -27,4 +31,40 @@ export const getTodasArtes = (req , res) => {
             message: `Não existem artes cadastradas`
           })
     }
-}
+    return res.status(200).send({
+        totalArtes: artes.length, artes
+      });
+    
+    };
+
+    //Função de obter uma arte por id
+    export const buscarArtePorId = (req, res) => {
+        const { id } = req.params;
+        const arte = list.buscarArtePorID(id);
+      
+        if (!arte) res.status(404).send({ message: "Arte não encontrada!" });
+      
+        return res.send(arte);
+      };
+     
+        //Função de criar uma arte
+    export const criarArtes = (req, res) => {
+        const { nome, imagem, tipo, obra } = req.body;
+      
+        if (!nome || !imagem || !tipo || !obra) {
+          return res
+            .status(400)
+            .send({ message: "Você deve informar o nome, imagem, tipo e obra!" });
+        }
+      
+        if (!verifyImage(imagem)) {
+          return res
+            .status(400)
+            .send({ message: "Você deve informar uma imagem válida!" });
+        }
+      
+        const arte = list.criarArte(nome, imagem, tipo, obra);
+      
+        return res.status(201).send(arte);
+      }
+    
