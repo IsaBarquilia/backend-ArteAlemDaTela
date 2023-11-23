ab28635bcd271c8624c57b700bb9b1bd5dabf6e2
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-11-23 15:26:45 -0300

    .

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import rotasArtes from "./arte.routes.js";
 import rotasSobreNos from "./sobrenos.routes.js"
 
 const router = Router()
-router.use("/artes", rotasArtes); //Rota para puxar as artes
+router.use("/", rotasArtes); //Rota para puxar as artes
 router.use("/sobrenos", rotasSobreNos); //Rota para puxar os integrantes
 
 router.get("/", (req, res) => {
--- src/routes/sobrenos.routes.js
+++ src/routes/sobrenos.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
-import { getTodosIntegrantes, getIntegrantesByid,editarIntegrante,deletarIntegrante,adicionarIntegrantes } from "../controllers/sobrenos.controller.js";
+import {
+     getTodosIntegrantes, 
+     getIntegrantesByid,
+     editarIntegrante,
+     deletarIntegrante,
+     adicionarIntegrantes 
+    } 
+    from "../controllers/sobrenos.controller.js";
 
 
-const routes = new Router();
+const rotasSobreNos = new Router();
 
 //rotas para puxar as informaçoes
-routes.get("/", getTodosIntegrantes);//rota todos os integrantes
-routes.get("/:id", getIntegrantesByid);// rota integrantes por id
-routes.put("/:id",editarIntegrante);// rota editar integrante por id
-routes.delete("/:id", deletarIntegrante);// rota deletar integrante por id
-routes.post("/", adicionarIntegrantes);//rota adicionar integrante
+rotasSobreNos.get("/", getTodosIntegrantes);//rota todos os integrantes
+rotasSobreNos.get("/:id", getIntegrantesByid);// rota integrantes por id
+rotasSobreNos.put("/:id",editarIntegrante);// rota editar integrante por id
+rotasSobreNos.delete("/:id", deletarIntegrante);// rota deletar integrante por id
+rotasSobreNos.post("/", adicionarIntegrantes);//rota adicionar integrante
+
+export default rotasSobreNos;
