1872b117a3fc889c2f14e399591d1b9994f13907
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-11-23 09:22:05 -0300

    .

--- src/routes/arte.routes.js
+++ src/routes/arte.routes.js
@@ -4,17 +4,11 @@ import { getTodasArtes, getArtesByid, criarArtes, editarArtes, deletarArte, busc
 const rotasArtes = Router();
 
 rotasArtes.get("/", getTodasArtes);
-
 rotasArtes.get("/:id", getArtesByid);
-
 rotasArtes.post("/", criarArtes);
-
 rotasArtes.put("/:id", editarArtes);
-
 rotasArtes.delete("/:id", deletarArte);
-
 rotasArtes.get("/:obra", buscarporObra);
-
 rotasArtes.get("/:tipo", buscarporTipo);
 
 export default rotasArtes;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import rotasArtes from "./arte.routes.js";
 
 const router = Router()
+router.use("/arte", rotasArtes)
 router.get("/", (req, res) => {
     return res.status(200).send({ message: "Rota ta funfionando" });
 });
