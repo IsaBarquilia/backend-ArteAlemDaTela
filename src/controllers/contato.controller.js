dc8cf09b615dbc4a1c1d9f3126117e8ede0673ec
Luana Fassini <luanafassini.564@gmail.com>
2023-11-28 13:45:38 -0300

    feat: finish page contato

--- src/controllers/contato.controller.js
+++ src/controllers/contato.controller.js
@@ -16,13 +16,15 @@ export function verificaemail(email){
     return verifica.includes(extensao);
 }
 
+//responsavel por adicionar mensagem do artista sendo necessario nome e email caso nao
+//for respondido aparecera mensagem de dados invalidos.
 export const adicionarArtista = (req, res) => {
     const {nome, email} = req.body;
-
+//verifica campos preenchidos
     if(!nome || !email){
         return res.status(400).send({message:"Dados inválidos"});
     }
-
+//verifica email valido
     if(!verificaemail(email)){
         return res.status(400).send({message:"Email inválido"});
     }
@@ -32,10 +34,11 @@ export const adicionarArtista = (req, res) => {
 //deletar integrantes caso o usuario informar id nao cadastrado retorna mensagem de integrante nao encontrado.
 
 export const deletarArtista = (req, res) => {
+//puxa o id do artista
     const {id} = req.params;
-
+//puxa funcao deletar artista pelo seu id
     const artista = sobre.deletarArtista(id);
-
+//verifica existencia do artista no sistema
     if(!artista){
         return res.status(404).send({message:"Artista não encontrado"});
     }
@@ -45,19 +48,20 @@ export const deletarArtista = (req, res) => {
     return res.status(200).send(artista);
 }
 
-//editar mensagem caso o artista tenha escrito algo errado.
+//editar mensagem caso o artista queira editar o que escreveu.
 export const editarMsg = (req, res) => {
+//puxar artista por id
     const {id} = req.params;
     const {nome, email} = req.body;
-
+//verifica campos preenchidos 
     if(!nome || !email){
         return res.status(400).send({message:"Dados inválidos"});
     }
-
+//puxa funcao que verifica nome
     if(!verificanome(nome)){
         return res.status(400).send({message:"Nome inválido"});
     }
-
+//puxa funcao que verifica email
     if(!verificaemail(email)){
         return res.status(400).send({message:"Email inválido"});
     }
--- src/routes/contato.routes.js
+++ src/routes/contato.routes.js
@@ -15,6 +15,6 @@ import {
     rotasContato.delete("/:id", deletarArtista);
     rotasContato.put("/:id", editarMsg);
     //rotas para teste de validação
-    rotasContato.get("/nome/:nome", verificanome);
-    rotasContato.get("/email/:email", verificaemail);
+    rotasContato.get("/:nome", verificanome);
+    rotasContato.get("/:email", verificaemail);
     export default rotasContato;
