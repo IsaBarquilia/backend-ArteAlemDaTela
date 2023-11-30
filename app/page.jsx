d60071bda0f80aa45acd00361f49f3a5bde2dbfb
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-11-30 10:35:17 -0300

    pasta

---
+++ app/page.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function pageHome() {
+  return (
+    <div>
+        <h1>Quadrinhos</h1>
+ <div className='quadroMaior'>
+    <div className='img'>
+
+    </div>
+    <div className='infos'>
+     <h2>nome da obra:</h2>
+     <h2>nome do artista:</h2>
+     <p>clique para saber mais</p>
+
+     <button>excluir</button>
+     <button>editar</button>
+
+     
+
+    </div>
+
+ </div>
+
+    </div>
+  )
+}
+export default pageHome
---
+++ pageTest.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+ 
+
+function pageTest() {
+  return (
+    <div>
+    <Link to= "/">ir para a pagina dos quadros</Link>
+    </div>
+  )
+}
+
+export default pageTest
