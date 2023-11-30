df4a15ab0bf62cb3358f08124001dfaa399cdcee
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-11-30 13:39:32 -0300

    datainformacoes

---
+++ src/data/quadros.js
@@ -0,0 +1,79 @@
+todosQuadros = [
+    {
+        nomeObra: "Noite estrelada",
+        artista: "Van Vogh",
+        dataProducao: "1889",
+        tipo: "Oleo sobre tela",
+        idadeArtista: "falecido com 37 anos",
+
+
+    },
+
+    {
+        nomeObra: "O Nascimento de Vênus",
+        artista: "Sandro Botticelli",
+        dataProducao: "1485",
+        tipo: "Pintura histórica",
+        idadeArtista: "falecido com 37 anos",
+    },
+ 
+
+    {
+        nomeObra: "O grito.",
+        artista: "Edvard Munch",
+        dataProducao: "1893",
+        tipo: "Óleo sobre tela",
+        idadeArtista: "falecido com 37 anos",
+
+    },
+    {
+        nomeObra: "Ivan, o Terrível",
+        artista: "Ilia Repin",
+        dataProducao: " 1883",
+        tipo: " Pintura histórica",
+        idadeArtista: "falecido com 86 anos",
+    },
+
+       
+
+    {
+        nomeObra: "A persistência da memória",
+        artista: "Salvador Dali.",
+        dataProducao: "1931",
+        tipo: "Alegoria",
+        idadeArtista: "falecido com 84 anos",
+    },
+
+    {
+        nomeObra: "Abaporu",
+        artista: "Tarsila do Amaral",
+        dataProducao: "1928",
+        tipo: "óleo sobre tela",
+        idadeArtista: "falecido com 86 anos",
+    },
+
+    {
+        nomeObra: "Britto Garden",
+        artista: "Romero Britt",
+        dataProducao: " 2000",
+        tipo: " Pintura histórica",
+        idadeArtista: "60 anos",
+    },
+
+    {
+        nomeObra: "Impressão, nascer do sol",
+        artista: "Claude Monet",
+        dataProducao: "1872",
+        tipo: "Tinta a óleo ",
+        idadeArtista: "falecido com 86 anos",
+    }
+    {
+        nomeObra: "Van goghAmendoeira em Flor",
+        artista: "Van gogh",
+        dataProducao: " 1890",
+        tipo: "Tinta a óleo ",
+        idadeArtista: "falecido com 37 anos",
+    }
+
+
+]
