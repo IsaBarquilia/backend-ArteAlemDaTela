afd52c639865f454dc6acfa2110f80f11df3237c
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-11-30 08:35:02 -0300

    Feat: finalizaçao do arquivo sobrenosList.js

--- src/models/sobrenosList.js
+++ src/models/sobrenosList.js
@@ -6,4 +6,26 @@ export class SobreNosList{
     getIntegrantes(){
         return this.integrantes;
     }
+
+    getIntegrantesPorId(id){
+        return this.integrantes.find((integrante) => integrante.id === id);
+    }
+
+    criarIntegrante(integrante){
+        this.integrantes.push(integrante);
+    }
+
+    editarIntegrante(id, nome, idade, imagem){
+        const integrante = this.getIntegrantesPorId(id);
+
+        if(integrante){
+            integrante.nome = nome;
+            integrante.idade = idade;
+            integrante.imagem = imagem;
+        }
+        return integrante;
+    }
+    deletarIntegrante(id){
+        this.integrantes = this.integrantes.filter((integrante) => integrante.id !== id);
+    }
 }
