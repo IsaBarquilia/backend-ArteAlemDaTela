6b64925bad07f1ea141f775a2147769d1d95c21e
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-11-30 08:43:54 -0300

    Feat: Inicio de controllers

--- src/controllers/sobrenos.controller.js
+++ src/controllers/sobrenos.controller.js
@@ -1,1 +1,13 @@
+import { SobreNosList } from "../models/sobrenosList.js";
+import { Sobrenos } from "../models/sobrenos.js";
 
+const sobrenosList = new SobreNosList();
+
+export const getIntegrantes = (req , res) =>{
+    const integrantes = sobrenosList.getIntegrantes();
+
+    if(integrantes.length){
+        return res.status(200).send(integrantes);
+    }
+    return res.status(404).send({message:"Não há integrantes cadastrados"});
+}
