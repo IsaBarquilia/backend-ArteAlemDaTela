fd963b5febd4ac2cb75cf899a59b2f195de484f0
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-11-30 08:58:30 -0300

    Feat: finalização controllers sobre nos

--- src/controllers/sobrenos.controller.js
+++ src/controllers/sobrenos.controller.js
@@ -10,4 +10,45 @@ export const getIntegrantes = (req , res) =>{
         return res.status(200).send(integrantes);
     }
     return res.status(404).send({message:"Não há integrantes cadastrados"});
+};
+
+export const getIntegrante = (req, res) =>{
+    const { id } = res.params;
+    const integrante = sobrenosList.getIntegrantesPorId(id);
+
+    if(integrante) res.status(404).send({message:"Integrante não encontrado"});
+
+    return res.send(integrante);
+};
+
+export const criarIntegrante = (req, res) =>{
+    const {nome, idade, imagem} = req.body;
+    const integrante = new Sobrenos(nome, idade, imagem);
+
+    sobrenosList.criarIntegrante(integrante);
+
+    return res.status(201).send(integrante);
+}
+
+export const editarIntegrante  = (res, res) =>{
+    const { id } = req.params;
+    const {nome, idade, imagem} = req.body;
+    const integrante = sobrenosList.getIntegrantesPorId(id);
+
+    if(!integrante)res.status(404).send({message:"Integrante não encontrado"});
+
+    sobrenosList.editarIntegrante(id, nome, idade, imagem);
+
+    return res.send(integrante);
+}
+
+export const deletarIntegrante = (req, res) =>{
+    const { id } = req.params;
+    const integrante = sobrenosList.getIntegrantesPorId(id);
+
+    if(!integrante) res.status(404).send({message:"Integrante não encontrado"});
+
+    sobrenosList.deletarIntegrante(id);
+
+    return res.send(integrante);
 }
