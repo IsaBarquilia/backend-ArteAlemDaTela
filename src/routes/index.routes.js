b9fe23d69c48f48c22a2d6054dc7cdbe9021064d
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-11-30 08:21:19 -0300

    Feat: Conserto de codigo

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -8,8 +8,6 @@ router.use("/", rotasArtes); //Rota para puxar as artes
 router.use("/sobrenos", rotasSobreNos); //Rota para puxar os integrantes
 router.use("/contato", contato); //Rota para puxar o contato
 
-router.get("/", (req, res) => {
-    return res.status(200).send({ message: "Rota está funcionando!" }); //Rota para ver se o servidor está funcionando
-});
+
 
 export default router;
