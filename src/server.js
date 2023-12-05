05a4db8cac14649edc0114528a97dcbed7988c94
IsaBarquilia <isabelle.barquilia.564@gmail.com>
2023-12-05 09:00:01 -0300

    ajustes

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import router from "./routes/index.routes.js";
 
 config(); // Carrega as variáveis de ambiente
 
-const port = process.env.PORT || 5000; // Pega a porta do arquivo .env ou usa a porta 5000
+const port = process.env.PORT || 3000; // Pega a porta do arquivo .env ou usa a porta 5000
 
 const app = express(); // Cria o servidor e armazena na variável app
 app.use(express.json()); // Configura o servidor para receber requisições com o formato JSON
