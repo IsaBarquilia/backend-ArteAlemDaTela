3b57aeeed5062e5462e9cad677452b7ab2bbd9ef
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 10:46:34 -0300

    feat: coment code

--- src/controllers/artes.controller.js
+++ src/controllers/artes.controller.js
@@ -1,10 +1,12 @@
 
-import obras from "../data/obras.js";
-import resgataArtes from "../models/ArteList.js"
-import Arte from "../models/arte.js"
+import obras from "../data/obras.js"; //importaçao pagina obras js
+import resgataArtes from "../models/ArteList.js"; //importaçao pagina ArteList js
+import Arte from "../models/arte.js"; //importaçao pagia obras js
 
+//cont responsavel em criar novas artes
 const arteLista = new resgataArtes()
 
+//mapea as artes
 obras.map(arte => new Arte(
   arte.nomeObra,
   arte.url,
@@ -12,6 +14,7 @@ obras.map(arte => new Arte(
   arte.artista ,
   arte.dataProducao,
   arte.tipo,
+
 )) .forEach(obras => arteLista.criarArtes(obras));
 
 
@@ -31,7 +34,7 @@ export const getTodasArtes = (req, res) => {
   let artes;
 
 
-
+  //Se 'tipo' estiver presente nos parâmetros de consulta
   if (tipo) {
     artes = arteLista.buscarporTipo(tipo);
     return res.status(200).send({
@@ -41,6 +44,8 @@ export const getTodasArtes = (req, res) => {
   } else {
     artes = arteLista.getTodasArtes();
   }
+//Se não houver artes cadastradas, o código retorna um erro 404 dizendo que não existem artes. 
+//Se houver, ele retorna um sucesso 200 com o número total de artes e a lista delas.
 
   if (artes.length === 0) {
     return res.status(404).send({
@@ -68,6 +73,9 @@ export const buscarArtePorID = (req, res) => {
     })
   }
 };
+
+//Ela extrai um parâmetro chamado dataProducao da requisição e usa esse parâmetro para encontrar uma arte específica. 
+//Se a arte não for encontrada, retorna um erro 404. Se encontrada, retorna a arte como resposta.
 export const dataProducao = (req, res) => {
   const { dataProducao } = req.params;
   const arteProducao = arteLista.dataProducao(dataProducao);
@@ -80,7 +88,9 @@ export const dataProducao = (req, res) => {
 
   return res.send(arteProducao);
 };
-//Função de criar uma arte
+//Esse código cria uma arte, verificando se os dados da requisição estão corretos. Ele checa campos obrigatórios, 
+//comprimento do texto, idade do artista, tamanho do texto e se a URL da imagem é válida. Se tudo estiver certo, 
+//adiciona a arte a uma lista. Se houver algum erro nos dados, retorna uma mensagem indicando o problema.
 export const criarArtes = (req, res) => {
 
   const { nomeObra, url, artista, dataProducao, tipo, idadeArtista } = req.body;
@@ -124,16 +134,25 @@ export const criarArtes = (req, res) => {
     erros.push('Imagem inválida');
   }
 
+  //Se houver erros nos dados da arte, o código retorna um status de erro 400 junto com uma lista de erros.
+  // Caso contrário, se os dados estiverem corretos, 
+  //ele adiciona a arte à lista e retorna um status de sucesso 201 com os detalhes da arte criada.
   if (numerosErros > 0) {
     return res.status(400).send({
       errors: erros,
-      status: "BAD REQUEST"
+      status: "ERRO"
     });
   } else {
     arteLista.criarArtes(arte);
     return res.status(201).send(arte);
   }
 };
+
+//Essa função busca uma arte pelo ID fornecido na requisição. 
+//Se a arte é encontrada, atualiza suas informações com os novos dados recebidos e retorna uma mensagem de 
+//sucesso com os detalhes da arte atualizada. Se a arte não é encontrada, retorna uma mensagem indicando que 
+//a arte não foi encontrada.
+
 
 export const editarArtes = (req, res) => {
   const { id } = req.params;
@@ -159,12 +178,15 @@ export const editarArtes = (req, res) => {
     arteAtualizada
   });
 };
+
+//Essa função busca a arte pelo ID fornecido. Se a arte é encontrada, ela é removida da lista de artes e uma mensagem confirmando a remoção é enviada como resposta. Se a arte não é encontrada, 
+//retorna uma mensagem indicando que a arte não foi encontrada.
 export const deletarArte = (req, res) => {
   const { id } = req.params;
   const arte = arteLista.buscarArtePorID(id);
 
-  if (!arte) res.status(404).send({ message: "arte não encontrada!" });
+  if (!arte) res.status(404).send({ message: "Arte não encontrada!" });
 
   arteLista.deletarArte(arte);
-  return res.send({ message: "arte removida com sucesso!" });
+  return res.send({ message: "Arte removida com sucesso!" });
 };
