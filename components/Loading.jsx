2741c356a46a1e83ae85e17f3f9802ee03cd9b2c
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 09:36:44 -0300

    feat: create loading 1

---
+++ components/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from '../loading/loading.module.css'
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+function ImageLoading() {
+
+    <Header/>
+    return (
+        <div id={styles.containerImgLoading}>
+                <img id={styles.image} src='/loading.gif' alt="Loading Image" />
+        </div>
+    )
+}
+
+<Footer/>
+
+export default ImageLoading;
