145a68028e34118508c65f97be72a232a43be808
Luana Fassini <luanafassini.564@gmail.com>
2023-12-05 14:38:23 -0300

    feat: coment code15

--- src/routes/sobrenos.routes.js
+++ src/routes/sobrenos.routes.js
@@ -1,4 +1,8 @@
 import { Router } from "express";
+
+//O código importa funções do arquivo sobrenos.controller.js que tratam das operações do grupo "Sobre Nós", 
+//incluindo obtenção de integrantes, busca por ID, criação, edição e exclusão de integrantes na aplicação.
+
 import { 
     getIntegrantes, 
     getIntegrantePorId, 
