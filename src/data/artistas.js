ad544b65e2124a994c718e72602cf25e75b34dde
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-12-05 07:56:09 -0300

    detalhes no codigo

--- src/data/artistas.js
+++ src/data/artistas.js
@@ -3,31 +3,31 @@ const artistas = [
         nome:"Isabela Alcântara",
         idade:"17 anos de idade",
         imagem:""
-    }
+    },
 
     {
         nome:"Isabelle Barquillia",
         idade:"16 anos de idade.",
         imagem:""
-    }
+    },
 
     {
         nome:"Marcelo Gabriel",
         idade:"17 anos de idade",
         imagem:""
-    }
+    },
 
     {
         nome:"Ana Julia",
         idade:"16 anos de idade",
         imagem:""
-    }
+    },
 
     {
         nome:"Raissa Godoy",
         idade:"18 anos de idade.",
         imagem:""
-    }
+    },
 
     {
         nome:"Luana Fassini",
