b20e0610f388e90c91442ddaf3297612320948dd
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-12-06 11:06:09 -0300

    add insta

--- src/data/artistas.js
+++ src/data/artistas.js
@@ -1,41 +1,47 @@
 
-//É um conjunto de objetos representando artistas. Cada um tem propriedades como nome, idade e 
+//É um conjunto de objetos representando artistas (que se refere aos programadores do site). Cada um tem propriedades como nome, idade e 
 //uma possível referência a uma imagem (URL vazia neste caso). 
 //Esses dados podem ser usados para trabalhar com informações sobre artistas em um programa.
 const Artistas = [
     {
         nome:"Isabela Alcântara",
         idade:"17 anos de idade",
+        insta:"",
         imagem:""
     },
 
     {
         nome:"Isabelle Barquillia",
         idade:"16 anos de idade.",
+        insta:"",
         imagem:""
     },
 
     {
         nome:"Marcelo Gabriel",
         idade:"17 anos de idade",
+        insta:"",
         imagem:""
     },
 
     {
         nome:"Ana Julia",
         idade:"16 anos de idade",
+        insta:"",
         imagem:""
     },
 
     {
         nome:"Raissa Godoy",
         idade:"18 anos de idade.",
+        insta:"",
         imagem:""
     },
 
     {
         nome:"Luana Fassini",
         idade:"16 anos de idade",
+        insta:"",
         imagem:""
     }
     
