38cac02f7c9f9f462c9aafbd4b807491da249074
Marcelo Lima <marcelo.lima4@aluno.senai.br>
2023-12-12 11:21:30 -0300

    Feat: Foto sobre nós

--- src/data/artistas.js
+++ src/data/artistas.js
@@ -6,37 +6,37 @@ const Artistas = [
     {
         nome:"Isabela Alcântara",
         idade:"17 anos de idade",
-        imagem:""
+        imagem:"blob:https://imgur.com/d5737dfe-08d5-40cf-91b1-1263f093833e"
     },
 
     {
         nome:"Isabelle Barquillia",
         idade:"16 anos de idade.",
-        imagem:""
+        imagem:"blob:https://imgur.com/226ad63c-184c-4019-9ab8-19dc9d556e80"
     },
 
     {
         nome:"Marcelo Gabriel",
         idade:"17 anos de idade",
-        imagem:""
+        imagem:"blob:https://imgur.com/8212d24d-a3e4-4a7c-9418-0a5176becd54"
     },
 
     {
-        nome:"Ana Julia",
+        nome:"Ana Júlia",
         idade:"16 anos de idade",
-        imagem:""
+        imagem:"blob:https://imgur.com/aff39a3d-d866-4663-b308-f7c3fea3afe8"
     },
 
     {
         nome:"Raissa Godoy",
         idade:"18 anos de idade.",
-        imagem:""
+        imagem:"blob:https://imgur.com/d1ec6547-744d-4cfe-8641-57642e9a643b"
     },
 
     {
         nome:"Luana Fassini",
         idade:"16 anos de idade",
-        imagem:""
+        imagem:"blob:https://imgur.com/d78eecdb-6977-4aad-8a5a-7a12a95e09e6"
     }
     
 ]
