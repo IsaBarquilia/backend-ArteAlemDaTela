eaeda59c86860774932cf48341686e1c37050d03
Belaalcantara <isabelaalcantara.senai564@gmail.com>
2023-12-14 01:40:50 -0300

    alteracao em obras

--- src/data/obras.js
+++ src/data/obras.js
@@ -7,9 +7,8 @@ const obras = [
         url: "  https://static.todamateria.com.br/upload/an/oi/anoiteestrelada-cke.jpg",
         artista: "Van Vogh",
         idadeArtista: "falecido com 37 anos",
-        descricao: "A obra Noite Estrelada é uma das pinturas mais icônicas de Vincent van Gogh, criada em 1889. Nela, o artista retrata uma paisagem noturna vibrante e imaginativa, onde o céu se funde em um turbilhão de cores intensas e ondulantes. As estrelas dançam sobre a pequena vila, enquanto ciprestes parecem se contorcer em movimentos sinuosos. Esta obra-prima expressa a intensidade emocional de van Gogh e sua conexão única com a natureza. A combinação de tons azuis profundos e amarelos vibrantes cria uma atmosfera envolvente e emocional, revelando a busca do artista pela beleza além da realidade visível. A Noite Estrelada é um testemunho duradouro da genialidade e da alma atormentada de van Gogh, que transcende o tempo, continuando a cativar e inspirar gerações.",
         dataProducao: "1889",
-        tipo: "Óleo sobre tela",
+        tipo: "Óleo sobre tela"
     },
 
     {
@@ -19,8 +18,7 @@ const obras = [
         idadeArtista: "falecido com 37 anos",
         dataProducao: "1485",
         tipo: "Pintura histórica", 
-        idadeArtista: "falecido com 37 anos",
-        descricao: "O Nascimento de Vênus é uma das obras-primas mais famosas de Sandro Botticelli, datada de meados dos anos 1480. Esta pintura renascentista retrata o momento mítico do nascimento da deusa Vênus, emergindo graciosa e completamente formada do mar em uma concha. A figura da deusa, representada de maneira serena e elegante, é um símbolo de beleza e perfeição clássica. Botticelli habilmente captura a harmonia entre a figura feminina e a natureza circundante, utilizando cores suaves e formas delicadas. Esta obra não apenas representa um momento mitológico, mas também encapsula a reverência pela estética clássica e pela feminilidade idealizada, tornando-se um ícone duradouro da arte renascentista italiana."
+        idadeArtista: "falecido com 37 anos"
 
     },
 
@@ -32,8 +30,7 @@ const obras = [
         idadeArtista: "falecido com 37 anos",
         dataProducao: "1893",
         tipo: "Óleo sobre tela",
-        idadeArtista: "falecido com 37 anos",
-        descricao: "O Grito de Edvard Munch é uma obra expressionista que se destaca pela intensidade emocional e pela representação angustiante da condição humana. A figura central, com um rosto distorcido, parece gritar silenciosamente em meio a um cenário surreal. As cores vibrantes e as linhas ondulantes contribuem para a atmosfera perturbadora e visceral da obra."
+        idadeArtista: "falecido com 37 anos"
 
     },
     {
@@ -44,8 +41,7 @@ const obras = [
         dataProducao: " 1883",
         tipo: " Pintura histórica",
 
-        idadeArtista: "falecido com 86 anos",
-        descricao: "Pintada por Ilia Repin, Ivan, o Terrível é uma obra que retrata o czar russo em um momento de introspecção e poder. A pintura histórica captura a personalidade complexa de Ivan IV, conhecido por seu reinado tumultuado. A expressão no rosto do czar e os detalhes da composição narram uma história rica em simbolismo e significado histórico."
+        idadeArtista: "falecido com 86 anos"
 
 
     },
@@ -60,8 +56,7 @@ const obras = [
         dataProducao: "1931",
         tipo: "Alegoria",
 
-        idadeArtista: "falecido com 84 anos",
-        descricao: " Salvador Dalí desafia a percepção do tempo em A Persistência da Memória. Os relógios derretidos representam a fluidez do tempo e a relatividade da realidade. Esta obra surrealista é uma representação visual impactante que convida o espectador a refletir sobre a natureza efêmera do tempo."
+        idadeArtista: "falecido com 84 anos"
 
     },
 
@@ -70,30 +65,21 @@ const obras = [
         url: "https://4.bp.blogspot.com/-DCRiGpFta4Y/WLMetsefC8I/AAAAAAAABHw/f7NYMWnFok8aV_JDssTw2rpanKe1sOOQACLcB/s1600/abaporu%2B-%2Bronperlim.com.br.jpg",
         artista: "Tarsila do Amaral",
         idadeArtista: "falecido com 86 anos",
-
-        descricao: "Criada por Tarsila do Amaral durante o movimento antropofágico brasileiro, Abaporu apresenta uma figura antropomórfica em um ambiente exótico. A obra reflete a fusão de influências modernistas e indígenas, destacando a identidade cultural única do Brasil.",
         dataProducao: "1928",
-
-        tipo: "Óleo sobre tela",
+        tipo: "Óleo sobre tela"
 
 
     },
 
     {
         nomeObra: "Britto Garden",
-
-        url: "https://www.mirabiledecor.com.br/media/catalog/product/cache/1/image/770x540/040ec09b1e35df139433887a97daa66f/0/0/00006pqd.jpg",
-        artista: "Romero Britt",
-        dataProducao: " 2000",
-        tipo: " Pintura histórica",
-        idadeArtista: "60 anos",
-        descricao: "Britto Garden de Romero Britto é uma explosão de cores e formas características da pop art contemporânea. A obra, criada em 2000, reflete o estilo vibrante e otimista do artista, com padrões geométricos e cores audaciosas que transmitem uma sensação de alegria e vitalidade.",
         url: "https://www.arteprints.com.br/wp-content/uploads/2014/09/Britto-Garden.jpg",
         artista: "Romero Britt",
-
-        idadeArtista: "60 anos",
         dataProducao: " 2000",
         tipo: " Pintura histórica",
+        artista: "Romero Britt",
+        idadeArtista: "60 anos",
+        tipo: " Pintura histórica"
 
     },
 
@@ -104,9 +90,7 @@ const obras = [
         idadeArtista: "falecido com 86 anos",
         dataProducao: "1872",
         tipo: "Tinta a óleo ",
-
-        idadeArtista: "falecido com 86 anos",
-        descricao: "Claude Monet, um dos principais artistas impressionistas, captura a atmosfera efêmera de um nascer de sol sobre a água em Impressão, Nascer do Sol. As pinceladas soltas e a paleta de cores suaves caracterizam a obra, que é uma representação emblemática do movimento impressionista"
+        idadeArtista: "falecido com 86 anos"
 
 
 
@@ -120,8 +104,7 @@ const obras = [
         dataProducao: " 1890",
         tipo: "Tinta a óleo ",
 
-        idadeArtista: "falecido com 37 anos",
-        descricao: "Van Gogh retrata a natureza com sua característica expressividade em Amendoeira em Flor. A obra, pintada em 1890, apresenta amendoeiras em flor com pinceladas vigorosas e cores vibrantes, refletindo a paixão do artista pela natureza."
+        idadeArtista: "falecido com 37 anos"
 
 
 
@@ -132,7 +115,6 @@ const obras = [
         url: "https://www.otempo.com.br/image/contentid/policy:1.2010717:1559857715/Capturar-JPG.JPG?f=3x2&w=1224",
         artista: "Leonardo da vinci",
         idadeArtista: "falecido com 67 anos",
-
         descricao: "Leonardo da Vinci eternizou a Mona Lisa com sua maestria técnica e habilidade de retratar a expressão humana. A enigmática mulher sorrindo, os detalhes meticulosos e a técnica sfumato contribuem para a aura misteriosa e atemporal desta obra-prima."
     },
 
@@ -140,36 +122,21 @@ const obras = [
         nomeObra: "Operários”",
         url: "https://blog.grafittiartes.com.br/wp-content/uploads/2020/02/operarios.jpg",
         artista: "Tarsila do Amaral",
-
         dataProducao: " 1890",
-
-        tipo: "Tinta a óleo",
-
-    },
-
-    {
-        nomeObra: "Operários",
-        url: "https://upload.wikimedia.org/wikipedia/pt/1/16/Operarios.jpg",
-        artista: "Tarsila do Amaral",
-        idadeArtista: "falecido com 86 anos",
-
-        dataProducao: "1933",
-        tipo: "Óleo sobre tela",
+        idadeArtista: "falecido com 68 anos",
+        tipo: "Tinta a óleo"
 
     },
+
+
     {
         nomeObra: "O Lavrador de Café",
 
         url: "https://blog.grafittiartes.com.br/wp-content/uploads/2020/10/O-Lavrador-de-Caf%C3%A9.jpg",
         artista: "Candido Portinari",
-
-
-        url: "https://assets.masp.org.br/uploads/collection/k3Si2GZdIUf7aumoDG5TircajN5MRG8N.jpg",
-        artista: "Candido Portinari",
         idadeArtista: "falecido com 58 anos",
-
         dataProducao: "1934",
-        tipo: "Óleo sobre tela",
+        tipo: "Óleo sobre tela"
 
     },
 
@@ -179,8 +146,7 @@ const obras = [
         artista: "Diego Velázquez ",
         idadeArtista: "falecido com 61 anos",
         dataProducao: "1656",
-
-        tipo: "Barroco",
+        tipo: "Barroco"
 
 
     },
@@ -191,8 +157,7 @@ const obras = [
         artista: "Di Cavalcanti",
         idadeArtista: " falecido com 79 anos",
         dataProducao: " 1925 ",
-
-        tipo: "Modernismo no Brasil",
+        tipo: "Modernismo no Brasil"
 
     },
 
@@ -202,7 +167,7 @@ const obras = [
         artista: "Diego Rivera",
         idadeArtista: "falecido com 70 anos",
         dataProducao: " 1935",
-        tipo: "Tinta a óleo ",
+        tipo: "Tinta a óleo "
 
     },
 
@@ -211,9 +176,8 @@ const obras = [
         url: "https://static.todamateria.com.br/upload/ob/ra/obras-de-salvador-dali-og.jpg",
         artista: "Salvador Dalí",
         idadeArtista: "falecido com 84 anos",
-
         dataProducao: " 1940 ",
-        tipo: " Óleo sobre tela",
+        tipo: " Óleo sobre tela"
 
 
     },
@@ -233,7 +197,7 @@ const obras = [
         artista: "Marcelo Santos",
         idadeArtista: " 53 anos",
         dataProducao: " 2010 ",
-        tipo: "Aquarela",
+        tipo: "Aquarela"
 
     },
 
